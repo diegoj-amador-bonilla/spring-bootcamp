92a99249295bb27122470224971cad3990807424
DIR\diego.amador.bonilla <diego.amador.bonilla@accenture.com>
2025-01-07 08:29:15 -0600

    Video #6 Dependency Injection using Spring Boot

---
+++ src/main/java/com/diego/demo/Alien.java
@@ -0,0 +1,14 @@
+package com.diego.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+    public void build() {
+
+
+        System.out.println("Working on something");
+    }
+
+}
--- src/main/java/com/diego/demo/DemoApplication.java
+++ src/main/java/com/diego/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.diego.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		Alien obj =context.getBean(Alien.class);
+
+		obj.build();
 	}
 
 }
