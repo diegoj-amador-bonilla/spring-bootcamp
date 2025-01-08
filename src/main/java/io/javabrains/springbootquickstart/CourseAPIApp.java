aa6137d4ea5edec42348c38a8a1eb5bb164113d2
DIR\diego.amador.bonilla <diego.amador.bonilla@accenture.com>
2025-01-08 10:14:51 -0600

    Initial Commit

--- src/main/java/io/javabrains/springbootquickstart/CourseAPIApp.java
+++ src/main/java/io/javabrains/springbootquickstart/CourseAPIApp.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Starter setup for Spring Boot Application from a Maven application
+//Took a fresh Maven Application and added the dependencies needed to make it a Spring Boot Application
 
 @SpringBootApplication
 public class CourseAPIApp {
