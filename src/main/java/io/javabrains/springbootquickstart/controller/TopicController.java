bc7dbd6c2eda89ac04f76423ee0aeca7d4d90686
DIR\diego.amador.bonilla <diego.amador.bonilla@accenture.com>
2025-01-09 11:53:49 -0600

    Videos 10-14 covering controller classes

---
+++ src/main/java/io/javabrains/springbootquickstart/controller/TopicController.java
@@ -0,0 +1,21 @@
+package io.javabrains.springbootquickstart.controller;
+
+import io.javabrains.springbootquickstart.data.Topics;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topics> getAllTopics(){
+        return Arrays.asList(
+                new Topics("spring", "Spring Framework", "Spring Framework Description"),
+                new Topics("java", "core java", "Core java Description"),
+                new Topics("javascript", "JavaScript", "JavaScript Description")
+        );
+    }
+}
