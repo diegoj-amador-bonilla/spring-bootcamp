3549e865d014624fb9093dad00cc9a27dd7ea888
DIR\diego.amador.bonilla <diego.amador.bonilla@accenture.com>
2025-01-21 07:01:13 -0600

    Spring Microservices Series Videos 5-7

--- src/main/java/io/javabrains/springbootquickstart/controller/HelloController.java
+++ src/main/java/io/javabrains/springbootquickstart/controller/HelloController.java
@@ -1,5 +1,7 @@
 package io.javabrains.springbootquickstart.controller;
 
+import io.javabrains.springbootquickstart.DbSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,22 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
+
 @RestController
 public class HelloController {
+    @Autowired
+    private DbSettings dbSettings;
 
-    @Value("${my.greeting: default value of HELLO}")
-    private String greetingMessage;
-
-    @Value("${my.listValues}")
-    private List<String> listValues;
-
-    @Value("#{${dbValues}}")
-    private Map<String, String> dbValues;
+//    @Value("${my.greeting: default value of HELLO}")
+//    private String greetingMessage;
+//
+//    @Value("${my.listValues:default value}")
+//    private List<String> listValues;
+//
+//    @Value("#{${dbValues:default value}}")
+//    private Map<String, String> dbValues;
 
 
     @RequestMapping("/hello")
     public String sayHi(){
-        return greetingMessage;
+        return dbSettings.getConnection() + dbSettings.getPort() + dbSettings.getHost();
     }
 
 }
