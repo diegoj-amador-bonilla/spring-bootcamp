69a64aa90d56012a42862b9d28b7b9e3e1c16cef
DIR\diego.amador.bonilla <diego.amador.bonilla@accenture.com>
2025-01-22 09:08:09 -0600

    Spring Microservices Series Videos 8-9

--- src/main/java/io/javabrains/springbootquickstart/controller/HelloController.java
+++ src/main/java/io/javabrains/springbootquickstart/controller/HelloController.java
@@ -3,6 +3,8 @@ package io.javabrains.springbootquickstart.controller;
 import io.javabrains.springbootquickstart.DbSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,9 @@ import java.util.Map;
 public class HelloController {
     @Autowired
     private DbSettings dbSettings;
+
+    @Autowired
+    private Environment env;
 
 //    @Value("${my.greeting: default value of HELLO}")
 //    private String greetingMessage;
@@ -30,4 +35,11 @@ public class HelloController {
         return dbSettings.getConnection() + dbSettings.getPort() + dbSettings.getHost();
     }
 
+    @GetMapping("/envdetails")
+    public String envDetails(){
+        return env.toString();
+    }
+
 }
+
+
